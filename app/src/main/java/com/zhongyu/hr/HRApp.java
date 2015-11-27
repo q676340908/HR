aa52519475cdaf60e95f3ea306f07eb1a87e8269
RabbpigPan <rabbpig2@gmail.com>
2015-11-27 16:00:49 +0800

    initial project

---
+++ app/src/main/java/com/zhongyu/hr/HRApp.java
@@ -0,0 +1,14 @@
+package com.zhongyu.hr;
+
+import com.orm.SugarApp;
+
+/**
+ * Created by Administrator on 2015/11/27.
+ */
+public class HRApp extends SugarApp{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
