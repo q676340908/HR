5705cd0a6435e5d47bd59460eb9a83fa1a3843ce
q676340908 <676340908@qq.com>
2015-11-28 00:20:19 +0800

    add loginActivity and serverSettingActivity

--- app/src/main/java/com/zhongyu/hr/LoginActivity.java
+++ app/src/main/java/com/zhongyu/hr/LoginActivity.java
@@ -1,13 +1,45 @@
 package com.zhongyu.hr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.setting_bt)
+    Button settingBt;
+    @Bind(R.id.username_et)
+    EditText usernameEt;
+    @Bind(R.id.passwd_et)
+    EditText passwdEt;
+    @Bind(R.id.remember_userid_cb)
+    CheckBox rememberUseridCb;
+    @Bind(R.id.remember_pwd_cb)
+    CheckBox rememberPwdCb;
+    @Bind(R.id.login_bt)
+    Button loginBt;
+
+    @OnClick(R.id.setting_bt)
+    public void goSetting(){
+
+        Intent intent = new Intent(LoginActivity.this,ServerSettingActivity.class);
+        startActivity(intent);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
 }
---
+++ app/src/main/java/com/zhongyu/hr/ServerSettingActivity.java
@@ -0,0 +1,15 @@
+package com.zhongyu.hr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hzz on 15/11/27.
+ */
+public class ServerSettingActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_setting);
+    }
+}
